c015a559096823dfc8a25a4c41bbad259fd720e0
Minato-hub <71182647+Minato-hub@users.noreply.github.com>
2025-02-02 22:20:27 +0900

    要素を画面中央に移動

--- FlutterTutorials/01FlutterFirstAppTutorial/namer_app/lib/main.dart
+++ FlutterTutorials/01FlutterFirstAppTutorial/namer_app/lib/main.dart
@@ -41,18 +41,21 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;  
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          BigCard(pair: pair),
-
-                    ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, 
+          children: [
+            // Text('A random AWESOME idea:'),
+            BigCard(pair: pair),
+              SizedBox(height: 30),
+              ElevatedButton(
+                  onPressed: () {
+                  appState.getNext();
+                },
+              child: Text('Next'),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -77,9 +80,12 @@ class BigCard extends StatelessWidget {
         elevation:5,
         color: theme.colorScheme.primary,   
         child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase, style: styleForCardText),
-      ),
+          padding: const EdgeInsets.all(20.0),
+          child: Text(
+          pair.asLowerCase, 
+          style: styleForCardText,
+          semanticsLabel: "${pair.first} ${pair.second}"),
+        ),
     );
   }
 }
