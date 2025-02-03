74e6640ee6ce0018d80704eb546db75084faa2b5
Minato-hub <71182647+Minato-hub@users.noreply.github.com>
2025-02-03 23:07:31 +0900

    選択されたメニューによって表示するページを切り替え可能に

--- FlutterTutorials/01FlutterFirstAppTutorial/namer_app/lib/main.dart
+++ FlutterTutorials/01FlutterFirstAppTutorial/namer_app/lib/main.dart
@@ -44,9 +44,29 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var selectedIndex = 0;     // ← Add this property.
+
   @override
   Widget build(BuildContext context) {
+    Widget page;
+      switch (selectedIndex) {
+        case 0:
+          page = GeneratorPage();
+          break;
+        case 1:
+          page = Placeholder();
+          break;
+        default:
+          throw UnimplementedError('no widget for $selectedIndex');
+      }
+
     return Scaffold(
       body: Row(
         children: [
@@ -63,9 +83,13 @@ class MyHomePage extends StatelessWidget {
                   label: Text('Favorites'),
                 ),
               ],
-              selectedIndex: 0,
+              selectedIndex: selectedIndex,    
               onDestinationSelected: (value) {
-                print('selected: $value');
+
+                setState(() {
+                  selectedIndex = value;
+                });
+
               },
             ),
           ),
@@ -73,7 +97,7 @@ class MyHomePage extends StatelessWidget {
           Expanded(
             child: Container(
               color: Theme.of(context).colorScheme.primaryContainer,
-              child: GeneratorPage(),
+              child: page, 
             ),
           ),
         ],
